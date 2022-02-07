c65654a8758a3bec415c9919ef1fe0f8073d6059
Carlos Cobian <rcobianm1600@alumno.ipn.x>
2022-02-07 11:52:13 -0600

    Implementación del modelo Order

--- HotCoffee/Models/Order.swift
+++ HotCoffee/Models/Order.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+enum CoffeeTypes: String, Codable{
+    case cappuccino
+    case latte
+    case espressino
+    case cortado
+}
+
+enum CoffeeSize: String, Codable{
+    case small
+    case medium
+    case large
+}
+
+struct Order{
+    let name: String
+    let email: String
+    let type: CoffeeTypes
+    let size: CoffeeSize
+}
