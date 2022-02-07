f2040cb618d7eb5f901e705ad00580f9073baa6e
Carlos Cobian <rcobianm1600@alumno.ipn.x>
2022-02-07 11:47:07 -0600

    Creación de clases para las UIControllers

---
+++ HotCoffee/Controllers/AddOrdersViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddOrdersViewController.swift
+//  HotCoffee
+//
+//  Created by Carlos Cobian on 07/02/22.
+//
+
+import Foundation
+import UIKit
+
+class AddOrdersViewController: UIViewController{
+    
+}
---
+++ HotCoffee/Models/Order.swift
@@ -0,0 +1,8 @@
+//
+//  Order.swift
+//  HotCoffee
+//
+//  Created by Carlos Cobian on 07/02/22.
+//
+
+import Foundation
